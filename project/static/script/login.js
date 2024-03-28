ba688bdb6f424747626c852ad2dd918e538fd329
muntyancraciunela@gmail.com <muntyancraciunela@gmail.com>
2024-03-28 22:57:22 +0200

    updated styling

--- project/static/script/login.js
+++ project/static/script/login.js
@@ -9,3 +9,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+// Obținem toate input-urile și label-urile
+const inputFields = document.querySelectorAll('.input-box input');
+
+// Pentru fiecare input, adăugăm un eveniment pentru a urmări schimbările
+inputFields.forEach(inputField => {
+    inputField.addEventListener('input', function () {
+        if (this.value.trim() !== '') {
+            this.parentNode.querySelector('label').classList.add('active');
+        } else {
+            this.parentNode.querySelector('label').classList.remove('active');
+        }
+    });
+});
